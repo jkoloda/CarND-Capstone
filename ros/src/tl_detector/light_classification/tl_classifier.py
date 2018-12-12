7f53c1b31563594d8c6289d795eb9afd8c8607bf
Jan Koloda <jan_koloda@hotmail.com>
2018-12-12 13:55:48 +0100

    Add TL detector

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,156 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import rospy
+
 
 class TLClassifier(object):
+    """Traffic light classifier based on a tensorflow model."""
+
     def __init__(self):
-        #TODO load classifier
-        pass
+        """Build, load and prepare traffic light classifier object."""
+        self.session = None
+        self.detection_graph = None
+
+        self.classes = {1: TrafficLight.GREEN,
+                        2: TrafficLight.RED,
+                        3: TrafficLight.YELLOW,
+                        4: TrafficLight.UNKNOWN}
+
+        self.model_path = None
+        self.load_model(model_path=self.model_path)
 
     def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+        """Determine the color of the traffic light in the image.
 
-        Args:
+        Args
+        ----
             image (cv::Mat): image containing the traffic light
 
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+        Returns
+        -------
+            int: ID of traffic light color
+                 (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        class_idx, confidence = self.predict(image)
+        return class_idx
+
+    def load_model(self, model_path):
+        """Load classifier (graph and session)."""
+        self.detection_graph = tf.Graph()
+        with tf.Session(graph=self.detection_graph) as sess:
+            self.session = sess
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(model_path, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+    def predict(self, image_np, min_score_thresh=0.5):
+        """Predict traffic light state from image.
+
+        Parameters
+        ----------
+        image_np : ndarray
+            Input image.
+
+        min_score_threshold : float
+            Confidence threshold for traffic light classification.
+
+        Returns
+        -------
+        light : TrafficLight
+            Light color of traffic light detected on input image.
+
+        score : float
+            Classification confidence score.
+
+        """
+        image_tensor = self.detection_graph.\
+            get_tensor_by_name('image_tensor:0')
+        detection_boxes = self.detection_graph.\
+            get_tensor_by_name('detection_boxes:0')
+        detection_scores = self.detection_graph.\
+            get_tensor_by_name('detection_scores:0')
+        detection_classes = self.detection_graph.\
+            get_tensor_by_name('detection_classes:0')
+        num_detections = self.detection_graph.\
+            get_tensor_by_name('num_detections:0')
+        image_np = self.process_image(image_np)
+
+        # Actual model prediction
+        input = [detection_boxes, detection_scores,
+                 detection_classes, num_detections]
+        (boxes, scores, classes) = self.session.run(
+                input,
+                feed_dict={image_tensor: np.expand_dims(image_np, axis=0)})
+
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes)
+        boxes = np.squeeze(boxes)
+
+        accumulated_scores = np.zeros(len(self.classes))
+        accumulated_classes = np.zeros(len(self.classes))
+        for ii, score in enumerate(scores):
+            if score > min_score_thresh:
+                # light_class = self.classes[classes[ii]]
+                # return light_class, score
+                accumulated_scores[classes[ii] - 1] += score
+                accumulated_classes[classes[ii] - 1] += 1
+
+        if np.sum(accumulated_scores) > 0:
+            light_class_idx = np.argmax(accumulated_scores) + 1
+            confidence = accumulated_scores[light_class_idx - 1] / \
+                float(accumulated_classes[light_class_idx - 1])
+
+            return self.classes[light_class_idx], confidence
+
+        else:
+            return None, None
+
+    def process_image(self, img):
+        """Pre-process imae so it can be passed directly to classifier.
+
+        Pre-processing consists of shrinkng the image to default maximum size
+        and converting in to RGB format (assuming that input is BGR).
+
+        Parameters
+        ----------
+        img : ndarray
+            Input image to be processed.
+
+        Returns
+        -------
+        img : ndarray
+            Processed image.
+
+        """
+        img = cv2.resize(img, (MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img
+
+    def shrink_image(self, img):
+        """Shrink image if bigger than default maximum dimensions.
+
+        Aspect ratio is kept. If the image is smaller it is return as it is.
+
+        Parameters
+        ----------
+        img : ndarray
+            Input image to be shrinked if necessary.
+
+        Returns
+        -------
+        img : ndarray
+            Shrinked image.
+
+        """
+        height, width = img.shape[:2]
+        if MAX_IMAGE_HEIGHT < height or MAX_IMAGE_WIDTH < width:
+            scaling_factor = np.min(MAX_IMAGE_HEIGHT / float(height),
+                                    MAX_IMAGE_WIDTH / float(width))
+            img = cv2.resize(img, None, fx=scaling_factor,
+                             fy=scaling_factor, interpolation=cv2.INTER_AREA)
+
+        return img
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -15,8 +15,12 @@ from scipy.spatial import KDTree
 
 STATE_COUNT_THRESHOLD = 3
 
+
 class TLDetector(object):
+    """Class to handle traffic light detection."""
+
     def __init__(self):
+        """Build TLDetector object."""
         rospy.init_node('tl_detector')
 
         self.pose = None
@@ -28,19 +32,23 @@ class TLDetector(object):
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         '''
-        /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
-        helps you acquire an accurate ground truth data source for the traffic light
-        classifier by sending the current color state of all traffic lights in the
-        simulator. When testing on the vehicle, the color state will not be available. You'll need to
-        rely on the position of the light and the camera image to predict it.
+        /vehicle/traffic_lights provides you with the location of the traffic
+        light in 3D map space and helps you acquire an accurate ground truth
+        data source for the traffic light classifier by sending the current
+        color state of all traffic lights in the simulator. When testing on the
+        vehicle, the color state will not be available. You'll need to rely on
+        the position of the light and the camera image to predict it.
         '''
-        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
+        sub3 = rospy.Subscriber('/vehicle/traffic_lights',
+                                TrafficLightArray,
+                                self.traffic_cb)
         sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
 
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
 
-        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
+        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint',
+                                                      Int32, queue_size=1)
 
         self.bridge = CvBridge()
         self.light_classifier = TLClassifier()
@@ -57,9 +65,11 @@ class TLDetector(object):
         rospy.spin()
 
     def pose_cb(self, msg):
+        """Car position callback."""
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
+        """Waypoints callback."""
         self.waypoints = waypoints
         # Base waypoints don't change so grabbing them once is enough
         if not self.waypoints_2d:
@@ -68,13 +78,15 @@ class TLDetector(object):
                                  for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d)
 
-
     def traffic_cb(self, msg):
+        """Traffic light callback."""
         self.lights = msg.lights
 
     def image_cb(self, msg):
-        """Identifies red lights in the incoming camera image and publishes the index
-            of the waypoint closest to the red light's stop line to /traffic_waypoint
+        """Identify red lights in the incoming camera image.
+
+        Publishes the index of the waypoint closest to the red light's
+        stop line to /traffic_waypoint.
 
         Args:
             msg (Image): image from car-mounted camera
@@ -102,29 +114,33 @@ class TLDetector(object):
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         self.state_count += 1
 
-
     def get_closest_waypoint(self, x, y):
-        """Identifies the closest path waypoint to the given position
+        """Identify the closest path waypoint to the given position.
+
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
-        Args:
+        Args
+        ----
             pose (Pose): position to match a waypoint to
 
-        Returns:
+        Returns
+        -------
             int: index of the closest waypoint in self.waypoints
 
         """
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
         return closest_idx
 
-
     def get_light_state(self, light):
-        """Determines the current color of the traffic light
+        """Determine the current color of the traffic light.
 
-        Args:
+        Args
+        ----
             light (TrafficLight): light to classify
 
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+        Returns
+        -------
+            int: ID of traffic light color
+                 (specified in styx_msgs/TrafficLight)
 
         """
         if(not self.has_image):
@@ -133,20 +149,23 @@ class TLDetector(object):
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        #Get classification
+        # Get classification
         # return self.light_classifier.get_classification(cv_image)
 
         # Return ground truth for debugging
         return light.state
 
-
     def process_traffic_lights(self):
-        """Finds closest visible traffic light, if one exists, and determines its
-            location and color
+        """Find closest visible traffic light if one exists.
 
-        Returns:
-            int: index of waypoint closes to the upcoming stop line for a traffic light (-1 if none exists)
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+        Determine also its location and color.
+
+        Returns
+        -------
+            int: index of waypoint closes to the upcoming stop line
+                 for a traffic light (-1 if none exists)
+            int: ID of traffic light color
+                 (specified in styx_msgs/TrafficLight)
 
         """
         light = None
@@ -154,7 +173,8 @@ class TLDetector(object):
         closest_light = None
         line_wp_idx = None
 
-        # List of positions that correspond to the line to stop in front of for a given intersection
+        # List of positions that correspond to the line to stop
+        # in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
             car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x,
@@ -177,8 +197,6 @@ class TLDetector(object):
                 return line_wp_idx, state
 
             return -1, TrafficLight.UNKNOWN
-
-
 
         if light:
             state = self.get_light_state(light)
