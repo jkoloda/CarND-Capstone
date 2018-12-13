79ccd31930f5aab307a16db7b6c799a2ea54dc41
Jan Koloda <jan_koloda@hotmail.com>
2018-12-13 12:49:52 -0800

    Adjust max throttle

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -20,8 +20,8 @@ class Controller(object):
         kp = 0.3
         ki = 0.1
         kd = 0.0
-        mn = 0      # Minimum throttle value
-        mx = 0.2    # Maximum throttle value
+        mn = 0       # Minimum throttle value
+        mx = 0.35    # Maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
 
         # Low-pass filter
